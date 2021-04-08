c1418754fb6d822cd5591bdc3efe0db7e8a48d6a
Heavenly Medina <medinah2@tcnj.edu>
2021-04-08 14:15:29 -0400

    slowed down game day/night cycle

--- Games 460 team 3/Assets/Scripts/Sun.cs
+++ Games 460 team 3/Assets/Scripts/Sun.cs
@@ -7,13 +7,13 @@ public class Sun : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(Vector3.zero, Vector3.right, 10f * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.right, 1f * Time.deltaTime);
         transform.LookAt(Vector3.zero);
     }
 }
