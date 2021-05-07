4690fb93d39813cee6264e2892efd6a7a2b74d35
Terell Powell <55817627+coolmankid12345@users.noreply.github.com>
2021-05-07 14:47:13 -0400

    Timer restart fix

--- Games 460 team 3/Assets/Scripts/PedestrianAI.cs
+++ Games 460 team 3/Assets/Scripts/PedestrianAI.cs
@@ -16,7 +16,7 @@ public class PedestrianAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     
+        night = false;
         animator = GetComponent<Animator>();
         velocityHash = Animator.StringToHash("Velocity");
         Ped = GetComponent<NavMeshAgent>();
